cb031193132e2628b020a11499ceb6c9e3370e0e
salakonrad <konradsala98@gmail.com>
2021-08-03 14:47:39 +0200

    feat(app): bs5 template + working auth

---
+++ smartschool/school/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=48, label="Nazwa użytkownika")
+    password = forms.CharField(max_length=64, label="Hasło")
