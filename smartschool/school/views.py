ab61ba652107cfb52c7f478cf118ea4ebe3faaf1
salakonrad <konradsala98@gmail.com>
2021-08-03 14:47:39 +0200

    feat(app): bs5 template + working auth

--- smartschool/school/views.py
+++ smartschool/school/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def index_view(request):
+    if request.user.is_authenticated:
+        return render(request, 'base.html')
+    return render(request, 'auth/login.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return index_view(request)
+            else:
+                return render(request, 'auth/login.html', {'status':'noauth'})
+        else:
+            return render(request, 'auth/login.html', {'status':'invalidform', 'error':form.errors})
+    else:
+        return render(request, 'auth/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return index_view(request)
