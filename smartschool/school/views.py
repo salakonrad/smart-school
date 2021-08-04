50a9a89d3cad0fc54c21704803bd42ea204d764d
salakonrad <konradsala98@gmail.com>
2021-08-04 17:54:23 +0200

    feat(profiles): add class profiles view

--- smartschool/school/views.py
+++ smartschool/school/views.py
@@ -7,11 +7,14 @@ from .forms import LoginForm, AddSquadForm, DeleteSquadForm, ChangeSquadForm
 from .models import Squad
 
 
+# /
 def index_view(request):
     if request.user.is_authenticated:
         return render(request, 'base.html')
     return render(request, 'auth/login.html')
 
+
+# /accounts/login
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -29,11 +32,15 @@ def login_view(request):
     else:
         return render(request, 'auth/login.html')
 
+
+# /accounts/logout
 @login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/accounts/login/')
 
+
+# /classes
 @login_required
 @permission_required('school.view_squad', raise_exception=True)
 def class_list_view(request, error_message=None):
@@ -45,6 +52,8 @@ def class_list_view(request, error_message=None):
     }
     return render(request, 'class/class_list.html', {'data': data})
 
+
+# /classes/add
 @login_required
 @permission_required('school.add_squad', raise_exception=True)
 def class_add(request):
@@ -65,6 +74,8 @@ def class_add(request):
     else:
         return class_list_view(request)
 
+
+# /classes/delete
 @login_required
 @permission_required('school.delete_squad', raise_exception=True)
 def class_delete(request):
@@ -79,6 +90,8 @@ def class_delete(request):
     else:
         return class_list_view(request)
 
+
+# /classes/change
 @login_required
 @permission_required('school.change_squad', raise_exception=True)
 def class_change(request):
@@ -100,3 +113,14 @@ def class_change(request):
             return class_list_view(request, form.errors)
     else:
         return class_list_view(request)
+
+
+# /class_profiles
+@login_required
+@permission_required('school.view_classprofile', raise_exception=True)
+def class_profiles_list_view(request, error_message=None):
+    data = {
+        'class_profiles': Squad.get_profiles(),
+        'errors': error_message
+    }
+    return render(request, 'class/class_profiles.html', {'data': data})
