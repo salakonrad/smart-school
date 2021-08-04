bb5c5ef048101fac83327cabe8526445c41648b3
salakonrad <konradsala98@gmail.com>
2021-08-04 15:30:30 +0200

    feat(class): adding and deleting classes

--- smartschool/school/forms.py
+++ smartschool/school/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=48, label="Nazwa użytkownika")
     password = forms.CharField(max_length=64, label="Hasło")
+
+class AddSquadForm(forms.Form):
+    name = forms.CharField(max_length=36)
+    profile = forms.IntegerField()
+    supervisor = forms.IntegerField()
+
+class DeleteSquadForm(forms.Form):
+    class_id = forms.IntegerField()
