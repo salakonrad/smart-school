247fd3216047ef25123e6edd262198d53b0e36c1
salakonrad <konradsala98@gmail.com>
2021-09-06 09:44:47 +0200

    Change Directories structure

--- smartschool/smartschool/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('', include('school.urls')),
-    path('admin/', admin.site.urls),
-]
